2a2b40a74e451b74428f3becedfa868aaf66fca7
Lauren W <lmw0165@gmail.com>
2022-05-04 11:44:08 -0500

    05-04-22 break up camel casing

---
+++ CW 05-04-2022/app.js
@@ -0,0 +1,19 @@
+// Complete the solution so that the function will break up camel casing, using a space between words.
+
+// Example
+
+// "camelCasing"  =>  "camel Casing"
+// "identifier"   =>  "identifier"
+// ""             =>  ""
+
+function solution(string) {
+    let res = "";
+    let letters = string.split("");
+    letters.forEach(letter=>{
+      if(letter.toUpperCase() == letter)
+        res+=" "+letter
+      else
+        res+=letter
+    })
+    return res;
+}
