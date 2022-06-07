5a152769d091146dfa149b8fc19be2faf076015f
Lauren W <lmw0165@gmail.com>
2022-06-06 22:46:30 -0500

    06-06-22 find error

---
+++ CW 06-06-2022/app.js
@@ -0,0 +1,11 @@
+// This code should store "codewa.rs" as a variable called name but it's not working. Can you figure out why?
+
+//before
+var a == "code";
+var b == "wa.rs";
+var name == a + b;
+
+//correction
+var a = "code";
+var b = "wa.rs";
+var name = a + b;
